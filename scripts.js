1428c76228d7cf73072c171505a789f8a7484395
gabesxr <gabrielyuripessoalx365@gmail.com>
2023-03-19 21:34:37 -0300

    add files.txt

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changecolor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function changeimg(imagem){
+    let imgIphone = document.querySelector(".iphone")
+imgIphone.src = imagem
+}
